95047d4e42d0416d1d9e355a086cfc95c67f1641
Badmapriya S <Badmapriya S@LAPTOP-26H7GP9H>
2025-10-08 12:35:58 +0530

    Updated Java exception handling
    Updated Java interface and wrapper class examples

---
+++ Tnsif_java/src/com/tnisf/day6/exceptionhandling/WithoutExceptionHandling.java
@@ -0,0 +1,12 @@
+//Program without exception handling
+
+package com.tnisf.day6.exceptionhandling;
+
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+}
