1ab3be44aef61402044044a623594c19e1a178c8
Badmapriya S <Badmapriya S@LAPTOP-26H7GP9H>
2025-10-25 15:49:19 +0530

    Update for Junit Testing

---
+++ Tnsif_java/src/com/tnisf/day10/junit/ParameterizedTestDemo.java
@@ -0,0 +1,19 @@
+package com.tnisf.day10.junit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParameterizedTestDemo {
+
+	
+	@ParameterizedTest
+	@ValueSource(strings= {"racecar","radar","madam","string"})
+	void testPalindromes(String word)
+	{
+		assertTrue(word.equals(new StringBuffer(word).reverse().toString()));
+	}
+	
+}
+
--- Tnsif_java/src/module-info.java
+++ Tnsif_java/src/module-info.java
@@ -5,4 +5,7 @@
  * 
  */
 module Tnsif_java {
+	requires junit;
+	requires org.junit.jupiter.api;
+	requires org.junit.jupiter.params;
 }
